ca29c265ba1ce9832ce5d5bd5680e3c930a71b06
chaimpan <chaim@chaimpaneth.com>
2018-06-03 16:10:04 +0300

    adding funcs for hebrew calendar support

--- src/interface.js
+++ src/interface.js
@@ -1,10 +1,19 @@
 const XDate = require('xdate');
+const Hebcal = require('hebcal');
 
 function padNumber(n) {
   if (n < 10) {
     return '0' + n;
   }
   return n;
+}
+
+function dateToData(date, isHebrewCal) {
+    if (isHebrewCal) {
+        return hdateToData(date);
+    } else {
+        return xdateToData(date);
+    }
 }
 
 function xdateToData(xdate) {
@@ -18,7 +27,29 @@ function xdateToData(xdate) {
   };
 }
 
-function parseDate(d) {
+function hdateToData(hdate) {
+    return {
+        year: hdate.getFullYear(),
+        month: hdate.getMonth(),
+        day: hdate.getDate(),
+        timestamp: hdate.greg().getTime(),
+        dateString: parseXDate(hdate.greg()).toString('yyyy-MM-dd'),
+    };
+}
+
+function hDateToMonthYear(date) {
+    return date.getFullYear().toString() + "-" + date.getMonth().toString() + "-" + date.getDate().toString();
+}
+
+function parseDate(d, isHebrewCal) {
+    if (isHebrewCal) {
+        return parseHDate(d);
+    } else {
+        return parseXDate(d)
+    }
+}
+
+function parseXDate(d) {
   if (!d) {
     return;
   } else if (d.timestamp) { // conventional data timestamp
@@ -36,8 +67,25 @@ function parseDate(d) {
   }
 }
 
-module.exports = {
-  xdateToData,
-  parseDate
-};
+function parseHDate(d) {
+    if (!d) {
+        return;
+    } else if (d.timestamp) { // conventional data timestamp
+        return Hebcal.HDate(new Date(d.timestamp));
+    } else if (typeof d === "string") {
+        return Hebcal.HDate(new Date(d));
+    } else if (d.getTime) { // javascript date
+        const dateString = d.getFullYear() + '-' + padNumber((d.getMonth() + 1)) + '-' + padNumber(d.getDate());
+        return Hebcal.HDate(new Date(dateString));
+    } else if (d.year) {
+        const dateString = d.year + '-' + padNumber(d.month) + '-' + padNumber(d.day);
+        return Hebcal.HDate(new Date(dateString));
+    } else if (d) { // timestamp nuber or date formatted as string
+        return Hebcal.HDate(d);
+    }
+}
 
+module.exports = {
+  dateToData,
+  parseDate,
+};
